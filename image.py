2449bbaa76e9ab863ef5889ad99cedda783b7139
A9-dev <henrypearson422@gmail.com>
2021-10-01 19:03:00 +0100

    Working test code to generate image with three lines of text

--- image.py
+++ image.py
@@ -6,8 +6,9 @@ artistName = "artistName"
 albumName = "albumName"
 img = Image.new('RGB', (800, 450), color='white')
 d = ImageDraw.Draw(img)
+font = ImageFont.truetype("Roboto/Roboto-Light.ttf", 75)
 d.text((10, 10), "%s\n%s\n%s" %
-       (songName, artistName, albumName), fill=(0, 255, 0), strokeWidth=125)
+       (songName, artistName, albumName), fill=(0, 255, 0), stroke_width=0, font=font)
 img.save('outputImage.png')
 # /Test code
 
