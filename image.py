7d3283308bd126ebadaa44a757b63628c14659fe
A9-dev <henrypearson422@gmail.com>
2021-10-01 20:08:38 +0100

    Added text wrapping

--- image.py
+++ image.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
-
+import textwrap
 imageSize = (800, 450)
-fontSize = 30
+fontSize = 35
 textOffset = (25, 25)
 backgroundColour = "white"
 albumCoverSize = (400, 400)
@@ -11,8 +11,12 @@ def generateImage(songName: str, artistName: str, albumName: str):
     img = Image.new('RGB', imageSize, color=backgroundColour)
     d = ImageDraw.Draw(img)
     font = ImageFont.truetype("Gotham-Font/GothamMedium.ttf", fontSize)
-    d.multiline_text(textOffset, "%s\n%s\n%s" %
-                     (songName, artistName, albumName), font=font, fill=(0, 0, 0))
+    textToWrite = [textwrap.wrap(i, width=550//fontSize)
+                   for i in [songName, artistName, albumName]]
+    textToWrite = sum(textToWrite, [])
+
+    d.multiline_text(textOffset, "\n".join(
+        textToWrite), font=font, fill=(0, 0, 0))
     albumCover = Image.open("testAlbumCover.jpg").resize(albumCoverSize)
     albumOffset = (img.height-albumCover.height)//2
     img.paste(albumCover, (img.width-albumCover.width-albumOffset, albumOffset))
