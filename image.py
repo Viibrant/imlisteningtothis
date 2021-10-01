9ca5a0b51e0ae05945854a57f646c4d79da9b1db
A9-dev <henrypearson422@gmail.com>
2021-10-01 19:10:01 +0100

    Updated test code

--- image.py
+++ image.py
@@ -1,16 +1,16 @@
 from PIL import Image, ImageDraw, ImageFont
 
-# Test code
-songName = "songName"
-artistName = "artistName"
-albumName = "albumName"
-img = Image.new('RGB', (800, 450), color='white')
-d = ImageDraw.Draw(img)
-font = ImageFont.truetype("Roboto/Roboto-Light.ttf", 75)
-d.text((10, 10), "%s\n%s\n%s" %
-       (songName, artistName, albumName), fill=(0, 255, 0), stroke_width=0, font=font)
-img.save('outputImage.png')
-# /Test code
+
+def generateImage(songName, artistName, albumName):
+    img = Image.new('RGB', (800, 450), color='white')
+    d = ImageDraw.Draw(img)
+    font = ImageFont.truetype("Roboto/Roboto-Light.ttf", 75)
+    d.multiline_text((10, 10), "%s\n%s\n%s" %
+                     (songName, artistName, albumName), font=font, fill=(0, 0, 0))
+    img.save('outputImage.png')
+
+
+generateImage("songName", "artistName", "albumName")
 
 
 def getImage(token: str, layout="compact"):
