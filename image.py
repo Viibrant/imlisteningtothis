bdf9ba2e670acdeacf968abd4418658a32312e0e
A9-dev <henrypearson422@gmail.com>
2021-10-01 19:40:25 +0100

    Added album cover pasting, resizing and dynamic y-axis centring

--- image.py
+++ image.py
@@ -2,20 +2,24 @@ from PIL import Image, ImageDraw, ImageFont
 
 imageSize = (800, 450)
 fontSize = 50
-textOffset = (10, 10)
+textOffset = (25, 25)
 backgroundColour = "white"
+albumCoverSize = (400, 400)
 
 
-def generateImage(songName, artistName, albumName):
+def generateImage(songName: str, artistName: str, albumName: str):
     img = Image.new('RGB', imageSize, color=backgroundColour)
     d = ImageDraw.Draw(img)
     font = ImageFont.truetype("Gotham-Font/GothamMedium.ttf", fontSize)
     d.multiline_text(textOffset, "%s\n%s\n%s" %
                      (songName, artistName, albumName), font=font, fill=(0, 0, 0))
+    albumCover = Image.open("testAlbumCover.jpg").resize(albumCoverSize)
+    albumOffset = (img.height-albumCover.height)//2
+    img.paste(albumCover, (img.width-albumCover.width-albumOffset, albumOffset))
     img.save('outputImage.png')
 
 
-generateImage("songName", "artistName", "albumName")
+generateImage("Blade", "Karma", "Anger Management")
 
 
 def getImage(token: str, layout="compact"):
