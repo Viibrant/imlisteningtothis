a11262a06837d09474f12bd345b2f0b335ec8891
Connor <connorbrook@googlemail.com>
2021-10-01 17:25:39 +0100

    initial endpoint

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_restful import Resource, Api
+from image import getImage
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+app = Flask(__name__)
+api = Api(app)
+class generateImage(Resource):
+    def get(self):
+        return getImage("foo")
+
+api.add_resource(generateImage, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
