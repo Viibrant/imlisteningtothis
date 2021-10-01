390696e1019b0d17893fabf8a51374f416d86bba
A9-dev <henrypearson422@gmail.com>
2021-10-01 18:44:53 +0100

    Added PIL import, requirement and added some test image generation code

--- image.py
+++ image.py
@@ -1,4 +1,15 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
+
+# Test code
+songName = "songName"
+artistName = "artistName"
+albumName = "albumName"
+img = Image.new('RGB', (800, 450), color='white')
+d = ImageDraw.Draw(img)
+d.text((10, 10), "%s\n%s\n%s" %
+       (songName, artistName, albumName), fill=(0, 255, 0), strokeWidth=125)
+img.save('outputImage.png')
+# /Test code
 
 
 def getImage(token: str, layout="compact"):
