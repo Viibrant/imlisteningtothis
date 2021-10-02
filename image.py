ef8de6fb24505348047be93890a9a545f05cc495
Connor <connorbrook@googlemail.com>
2021-10-02 12:03:30 +0100

    changed variable & function names to fit conventions

--- image.py
+++ image.py
@@ -4,34 +4,34 @@ import requests
 from io import BytesIO
 import base64
 
-imageSize = (800, 450)
-fontSize = 35
-textOffset = (25, 25)
-backgroundColour = "white"
-albumCoverSize = (400, 400)
+image_size = (800, 450)
+font_size = 35
+text_offset = (25, 25)
+background_colour = "white"
+album_cover_size = (400, 400)
 
 
-def generateImage(songName: str, artistName: str, albumName: str, albumCoverURL: str):
-    img = Image.new("RGB", imageSize, color=backgroundColour)
+def generate_image(song_name: str, artist_name: str, album_name: str, album_url: str):
+    img = Image.new("RGB", image_size, color=background_colour)
     d = ImageDraw.Draw(img)
-    font = ImageFont.truetype("Gotham-Font/GothamMedium.ttf", fontSize)
-    textToWrite = [
-        textwrap.wrap(i, width=550 // fontSize)
-        for i in [songName, artistName, albumName]
+    font = ImageFont.truetype("Gotham-Font/GothamMedium.ttf", font_size)
+    text_to_write = [
+        textwrap.wrap(i, width=550 // font_size)
+        for i in [song_name, artist_name, album_name]
     ]
-    textToWrite = sum(textToWrite, [])
-    d.multiline_text(textOffset, "\n".join(textToWrite), font=font, fill=(0, 0, 0))
-    response = requests.get(albumCoverURL)
-    albumCover = Image.open(BytesIO(response.content)).resize(albumCoverSize)
-    albumOffset = (img.height - albumCover.height) // 2
-    img.paste(albumCover, (img.width - albumCover.width - albumOffset, albumOffset))
+    text_to_write = sum(text_to_write, [])
+    d.multiline_text(text_offset, "\n".join(text_to_write), font=font, fill=(0, 0, 0))
+    response = requests.get(album_url)
+    album_cover = Image.open(BytesIO(response.content)).resize(album_cover_size)
+    album_offset = (img.height - album_cover.height) // 2
+    img.paste(album_cover, (img.width - album_cover.width - album_offset, album_offset))
     buffered = BytesIO()
     img.save(buffered, format="JPEG")
-    imgStr = base64.b64encode(buffered.getvalue())
-    return imgStr
+    img_str = base64.b64encode(buffered.getvalue())
+    return img_str
 
 
-generatedImage = generateImage(
+generatedImage = generate_image(
     "Song Name",
     "Artist Name",
     "Album Name",
@@ -39,5 +39,5 @@ generatedImage = generateImage(
 )
 
 
-def getImage(token: str, layout="compact"):
+def get_image(token: str, layout="compact"):
     pass
