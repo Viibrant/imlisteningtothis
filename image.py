1b5bcf1c6ff057986fe1431ea49d66b543828e55
Connor <connorbrook@googlemail.com>
2021-10-02 11:57:24 +0100

    formatted with black

--- image.py
+++ image.py
@@ -12,26 +12,31 @@ albumCoverSize = (400, 400)
 
 
 def generateImage(songName: str, artistName: str, albumName: str, albumCoverURL: str):
-    img = Image.new('RGB', imageSize, color=backgroundColour)
+    img = Image.new("RGB", imageSize, color=backgroundColour)
     d = ImageDraw.Draw(img)
     font = ImageFont.truetype("Gotham-Font/GothamMedium.ttf", fontSize)
-    textToWrite = [textwrap.wrap(i, width=550//fontSize)
-                   for i in [songName, artistName, albumName]]
+    textToWrite = [
+        textwrap.wrap(i, width=550 // fontSize)
+        for i in [songName, artistName, albumName]
+    ]
     textToWrite = sum(textToWrite, [])
-    d.multiline_text(textOffset, "\n".join(
-        textToWrite), font=font, fill=(0, 0, 0))
+    d.multiline_text(textOffset, "\n".join(textToWrite), font=font, fill=(0, 0, 0))
     response = requests.get(albumCoverURL)
     albumCover = Image.open(BytesIO(response.content)).resize(albumCoverSize)
-    albumOffset = (img.height-albumCover.height)//2
-    img.paste(albumCover, (img.width-albumCover.width-albumOffset, albumOffset))
+    albumOffset = (img.height - albumCover.height) // 2
+    img.paste(albumCover, (img.width - albumCover.width - albumOffset, albumOffset))
     buffered = BytesIO()
     img.save(buffered, format="JPEG")
     imgStr = base64.b64encode(buffered.getvalue())
     return imgStr
 
 
-generatedImage = generateImage("Song Name", "Artist Name", "Album Name",
-                               "https://images-na.ssl-images-amazon.com/images/I/61dGdORObgL._AC_.jpg")
+generatedImage = generateImage(
+    "Song Name",
+    "Artist Name",
+    "Album Name",
+    "https://images-na.ssl-images-amazon.com/images/I/61dGdORObgL._AC_.jpg",
+)
 
 
 def getImage(token: str, layout="compact"):
