4c7ac119eef353eba8838df6a252d5d3cf4af35f
Viibrant <11376396+Viibrant@users.noreply.github.com>
2024-01-04 22:48:59 +0000

    Example endpoint to test nodemon

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const next = require('next');
+const express = require("express");
+const next = require("next");
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express();
+	const server = express();
 
-  // Define your custom server routes here
+	// Define your custom server routes here
+	server.get("/api/hello", (req, res) => {
+		res.json({ message: "Hello World ;)!" });
+	});
 
-  server.get('*', (req, res) => {
-    return handle(req, res);
-  });
+	server.get("*", (req, res) => {
+		return handle(req, res);
+	});
 
-  server.listen(3000, (err) => {
-    if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
-  });
+	server.listen(3000, (err) => {
+		if (err) throw err;
+		console.log("> Ready on http://localhost:3000");
+	});
 });
